873db6b7b2a7145a86294d30c1c69c533c854ecd
Aghosh <aghosh@gmail.com>
2026-03-27 17:48:13 +0530

    git commit -m " fix for allowed host"

--- shelfAnalyzer/shelfAnalyzer/settings.py
+++ shelfAnalyzer/shelfAnalyzer/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
